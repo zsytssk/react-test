aaa152847aef8e00d664b6e38ec20c457aaf0b12
zsytssk <zsytssk@gmail.com>
2020-04-20 10:03:45 +0800

    master:

--- src/app/less/scss.tsx
+++ src/app/less/scss.tsx
@@ -1,16 +1,18 @@
 import React, { useRef } from 'react';
+import classnames from 'classnames';
 import style from './scss.less';
 
 export const UseLessTest = () => {
 	const ref1 = useRef<HTMLDivElement>(null);
 	const ref2 = useRef<HTMLDivElement>(null);
 
+	console.log(style);
 	return (
 		<div className={style.box}>
 			<div ref={ref1} className={style.child}>
 				<div> line1</div>
 			</div>
-			<div className={style.child}>line2</div>
+			<div className={classnames({ [style.class]: true })}>line2</div>
 		</div>
 	);
 };
