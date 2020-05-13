35176512b921ce9c4e764a856555a2814ab61fb7
zsytssk <zsytssk@gmail.com>
2020-05-13 20:58:06 +0800

    master:[add] GetChildState

--- src/app/advance/getChildState.tsx
+++ src/app/advance/getChildState.tsx
@@ -2,32 +2,39 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
+type PaState = { count: number };
+type State = Partial<PaState>;
 export function GetChildState() {
-	const [count, setCount] = useState(0);
+	const [state, setState] = useState<State>();
 
 	return (
 		<>
 			<Child
-				onChange={(state) => {
-					setCount(state);
+				onChange={(change_state) => {
+					setState({ ...state, ...change_state });
 				}}
+				state={state}
 			/>
-			<div>{count}</div>
+			<div>{state?.count}</div>
 		</>
 	);
 }
 
 type Props = {
-	onChange: (state: number) => void;
+	onChange: (state: State) => void;
+	state: State;
 };
 export function Child(props: Props) {
-	const { onChange } = props;
-	const [state, setState] = useState(0);
+	const { onChange, state: p_state } = props;
+	const [state, setState] = useState(p_state?.count || 0);
 
 	useEffect(() => {
 		if (onChange) {
-			onChange(state);
+			onChange({
+				count: state,
+			});
 		}
+		console.log(state);
 	}, [state]);
 
 	return (
