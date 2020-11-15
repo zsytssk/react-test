5eb3013397868ea771e86fc5773febd8851521c7
zsytssk <zsytssk@gmail.com>
2020-11-15 10:51:48 +0800

    master:

--- src/app/stateTest/stateManger.ts
+++ src/app/stateTest/stateManger.ts
@@ -1,5 +1,5 @@
 import { EventCom } from '../eventCom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { sleep } from '@app/utils/utils';
 import { ItemModal } from './itemModal';
 
@@ -20,26 +20,66 @@ class StateModel extends EventCom {
 
 let state = new StateModel();
 
-export function createUseState<T extends EventCom>(state: T, eventList: string[]) {
+export const getState = createUseModal(state, [StateEvent.ChangeCount, StateEvent.ChangeTest]);
+
+export function useModel<T extends EventCom>(model: T, eventList: string[]) {
+	const ref = useRef<() => void>();
+	const [changeIndex, setChangeIndex] = useState(0);
+
+	useEffect(() => {
+		ref.current = () => {
+			setChangeIndex(changeIndex + 1);
+		};
+		return () => {
+			ref.current = undefined;
+		};
+	}, [changeIndex]);
+
+	useEffect(() => {
+		const fn = () => {
+			ref.current?.();
+		};
+		for (const event of eventList) {
+			model.on(event, fn);
+		}
+		return () => {
+			for (const event of eventList) {
+				model.off(event, fn);
+			}
+		};
+	}, [model, eventList]);
+
+	return [model, changeIndex] as [T, number];
+}
+
+export function createUseModal<T extends EventCom>(model: T, eventList: string[]) {
 	return () => {
+		const ref = useRef<() => void>();
 		const [changeIndex, setChangeIndex] = useState(0);
 
 		useEffect(() => {
-			const fn = () => {
+			ref.current = () => {
 				setChangeIndex(changeIndex + 1);
 			};
-			for (const event of eventList) {
-				state.on(event, fn);
-			}
 			return () => {
-				for (const event of eventList) {
-					state.off(event, fn);
-				}
+				ref.current = undefined;
 			};
 		}, [changeIndex]);
 
-		return [state, changeIndex] as [T, number];
+		useEffect(() => {
+			const fn = () => {
+				ref.current?.();
+			};
+			for (const event of eventList) {
+				model.on(event, fn);
+			}
+			return () => {
+				for (const event of eventList) {
+					model.off(event, fn);
+				}
+			};
+		}, [model, eventList]);
+
+		return [model, changeIndex] as [T, number];
 	};
 }
-
-export const getState = createUseState(state, [StateEvent.ChangeCount, StateEvent.ChangeTest]);
